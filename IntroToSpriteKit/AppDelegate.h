73405cd1dc46bba39ff803886c46a9c0509bc2a1
Leveton <mleveton@gmail.com>
2015-04-08 13:00:28 -0400

    Initial Commit

---
+++ IntroToSpriteKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IntroToSpriteKit
+//
+//  Created by Mike Leveton on 4/8/15.
+//  Copyright (c) 2015 MEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IntroToSpriteKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IntroToSpriteKit
+//
+//  Created by Mike Leveton on 4/8/15.
+//  Copyright (c) 2015 MEL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
